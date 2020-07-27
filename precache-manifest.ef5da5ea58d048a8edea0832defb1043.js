c86bff08d7128e675886b905b6c08c7acb963c32
yiyiingit <jerryyiyi@126.com>
2020-07-27 16:15:19 -0700

    Updates

---
+++ precache-manifest.ef5da5ea58d048a8edea0832defb1043.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e8dfac5d2bc55114baf1e7dd3903ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04408a49ade9b4cc56eb",
+    "url": "/static/css/main.10ccdaea.chunk.css"
+  },
+  {
+    "revision": "d76fe918e3e504c35130",
+    "url": "/static/js/2.d3643521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d3643521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04408a49ade9b4cc56eb",
+    "url": "/static/js/main.7ae588b1.chunk.js"
+  },
+  {
+    "revision": "cb7abcf8d3088f151358",
+    "url": "/static/js/runtime-main.df7f9a4d.js"
+  },
+  {
+    "revision": "05360875fa3508b7096ddc620e62cce9",
+    "url": "/static/media/spinner.05360875.svg"
+  },
+  {
+    "revision": "11bbcf008576d8c68f9098bd60073b95",
+    "url": "/static/media/unavailable.11bbcf00.jpg"
+  }
+]);
