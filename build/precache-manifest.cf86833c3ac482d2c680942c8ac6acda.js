41767658acd8d174e1902a5cd98f78dcc06c815b
yiyiingit <jerryyiyi@126.com>
2020-07-27 16:16:55 -0700

    set up gh-pages deployment

---
+++ build/precache-manifest.cf86833c3ac482d2c680942c8ac6acda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef1282154bc652dae256f9ba2324913",
+    "url": "/movieSearch/index.html"
+  },
+  {
+    "revision": "224e34e95949f657e2a2",
+    "url": "/movieSearch/static/css/main.10ccdaea.chunk.css"
+  },
+  {
+    "revision": "d76fe918e3e504c35130",
+    "url": "/movieSearch/static/js/2.d3643521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieSearch/static/js/2.d3643521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224e34e95949f657e2a2",
+    "url": "/movieSearch/static/js/main.9021e0cb.chunk.js"
+  },
+  {
+    "revision": "a176775fce4048adaa0a",
+    "url": "/movieSearch/static/js/runtime-main.5d9c92cf.js"
+  },
+  {
+    "revision": "05360875fa3508b7096ddc620e62cce9",
+    "url": "/movieSearch/static/media/spinner.05360875.svg"
+  },
+  {
+    "revision": "11bbcf008576d8c68f9098bd60073b95",
+    "url": "/movieSearch/static/media/unavailable.11bbcf00.jpg"
+  }
+]);
